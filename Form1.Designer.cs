6b7d71599d164da062c4287e2fa327905bdb82fd
Budoc53 GG <viot.g.53@orange.fr>
2020-11-27 23:05:18 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Arbres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.tbChoixNoeud = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(388, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Création arbre binaire à 5 niveaux";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(407, 131);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(121, 276);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.Visible = false;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(611, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(49, 548);
+            this.listBox2.TabIndex = 5;
+            // 
+            // tbChoixNoeud
+            // 
+            this.tbChoixNoeud.Location = new System.Drawing.Point(407, 423);
+            this.tbChoixNoeud.Name = "tbChoixNoeud";
+            this.tbChoixNoeud.Size = new System.Drawing.Size(121, 22);
+            this.tbChoixNoeud.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 556);
+            this.Controls.Add(this.tbChoixNoeud);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox tbChoixNoeud;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arbres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //================================
+        private bool RacineOk=false;
+        int[] Arbre = new int[33];
+        //================================
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = (Math.Pow(2,5) + 1).ToString(); //33           
+            for(int i=0; i<33;i++) //initialise le tableau
+            {
+                Arbre[i] = -1;
+            }
+            label2.Text = "Racine? (entier)";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+        void ActualiseListeNoeud()
+        {           
+            listBox1.Items.Clear();
+            for (int i=0; i<33;i++)
+            {
+                if (Arbre[i]!=-1) {
+                    listBox1.Items.Add(Arbre[i]);
+                //    MessageBox.Show(Arbre[i].ToString());
+                }
+            }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+           
+        }
+        private void AfficheTablo()
+        {
+            listBox2.Items.Clear();
+          
+            for (int i = 0; i < 33; i++)
+            {
+                listBox2.Items.Add(Arbre[i].ToString()); 
+            }
+        }
+        int ValeurDuNoeud, noeudPere;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {               }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            tbChoixNoeud.Text = listBox1.SelectedItem.ToString();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+       {
+            if(e.KeyCode==Keys.Enter)  // si le contenu du texbox est validé par ENTER
+            {
+                if (!RacineOk) { 
+                    Arbre[0] = int.Parse(textBox1.Text);
+                    AfficheTablo();
+                    label1.Text = "Pour créer un noeud cliquez le père dans la liste puis indiquez la valeur puis Valider";
+                    label2.Text="Valeur du noeud";
+                    RacineOk = true;
+                    listBox1.Visible = true;
+                    ActualiseListeNoeud();
+                }
+                else
+                {
+                    AfficheTablo();
+                    ActualiseListeNoeud();
+                    // ajoute le noeud
+                    ValeurDuNoeud= int.Parse(textBox1.Text);
+                    noeudPere = int.Parse( tbChoixNoeud.Text);
+                    int k = -1;
+                    do
+                    {
+                        k++;
+                    } while (noeudPere != Arbre[k]);
+                    // reste à tester si le N G existe et postionner à droite
+                    // Si 2 N existent proposer l'effacement  d'un noeud
+                    Arbre[2 * k + 1] = ValeurDuNoeud;// Création du noeud gauche
+                    AfficheTablo();
+                    ActualiseListeNoeud();
+                }
+            }
+        }
+    }
+}
